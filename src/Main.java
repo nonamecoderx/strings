ffb2cdffde58eca669a7c8ca220ab2d528852c34
Иван <ivankiseliov@rambler.ru>
2022-09-29 20:38:26 +0300

    dz 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        //задание 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        //задание 3
+        String newFullName = "Иванов Семён Семёнович";
+        String replacedString = newFullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + replacedString);
     }
 }
+
